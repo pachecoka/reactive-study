3a620293ea2925b070f2572dd7323c6b1585f3a0
Leonardo Chassot <leonardo.c.marques3@gmail.com>
2022-01-14 18:52:45 -0300

    Doing tests  with the properties and configmap

--- src/test/java/com/study/webflux/WebfluxApplicationTests.java
+++ src/test/java/com/study/webflux/WebfluxApplicationTests.java
@@ -2,8 +2,10 @@ package com.study.webflux;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class WebfluxApplicationTests {
 
 	@Test
